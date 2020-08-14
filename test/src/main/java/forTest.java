6884590f81c082c1218232dff62256bae1910586
chunLinssp <934883425@qq.com>
2020-08-14 22:44:58 +0800

    for test

--- test/src/main/java/forTest.java
+++ test/src/main/java/forTest.java
@@ -3,6 +3,45 @@ public class forTest {
     public static void main(String[] args) {
         System.out.println("yes");
         System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
+        System.out.println("yes");
     }
 
 }
